a3cedcbbb0b81255e5841532480d6ae2e7e7f726
Peacho <emahilmo@>
2023-09-20 21:44:06 -0600

    Added name, job and the display for the jobs

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -5,4 +5,15 @@ public class Resume
     public string _name;
     public List<Job>_jobs = new List<Job>();
 
+    public void ResumeDisplay()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs:");
+
+        foreach(Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+
 }
