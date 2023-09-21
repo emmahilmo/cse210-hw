0ebcdd2c1d7d72414ab04573c297b57ca5060db9
Peacho <emahilmo@>
2023-09-20 20:04:15 -0600

    Added jobs 1 and 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Alcatraz Escape Games";
+        job1._jobTitle = "Shift Lead";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+
+        Job job2 = new Job();
+        job2._company = "Realities Edge Space Center";
+        job2._jobTitle = "Second Chair";
+        job2._startYear = "2018";
+        job2._endYear = "2021";
     }
 }
