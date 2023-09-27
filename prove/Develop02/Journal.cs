85aa67b6f30b1882b5cbdb24e7f3b094e230559d
Peacho <emahilmo@>
2023-09-27 10:45:33 -0600

    Added Class Journal and adding entries

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    public List<Entry>_entries = new List<Entry>();
+    public void addEntry(Entry new Entry) 
+    {
+        _entries.Add(newEntry);
+    }
+
+public class Entry
+{
+}
+
+public void DisplayAll()
+    {
+        foreach (Entry)
+    }
+}
