3f9317cc5673bd3ec648221b4663f6907faa8c58
Peacho <emahilmo@>
2023-09-26 19:19:40 -0600

    Added a welcome, directions and part of while

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program");
+
+        
+        int choice = -1;
+
+        while(choice != 5);
+
+        
     }
+    
 }
+
+
+
+//Write a new entry - Show the user a random prompt (from a list that you create), 
+//and save their response, the prompt, and the date as an Entry.
+//Display the journal - Iterate through all entries in the journal and display them to the screen.
+//Save the journal to a file - Prompt the user for a filename and then save the current journal 
+//(the complete list of entries) to that file location.
+//Load the journal from a file - Prompt the user for a filename and then load the journal
+//(a complete list of entries) from that file. This should replace any entries currently stored the journal.
+//Provide a menu that allows the user choose these options
+//Your list of prompts must contain at least five different prompts. Make sure to add your own prompts to the list, 
+//but the following are examples to help get you started:
+//Who was the most interesting person I interacted with today?
+//What was the best part of my day?
+//How did I see the hand of the Lord in my life today?
+//What was the strongest emotion I felt today?
+//If I had one thing I could do over today, what would it be?
+
+//Design requirements:
+//Contain classes for the major components in the program.
+//Contain at least two classes in addition to the Program class.
+//Demonstrate the principle of abstraction by using member variables and methods appropriately.
