a3d341e31971718f7451a0a1dd85bbb0667936f4
Peacho <emahilmo@>
2023-09-27 08:43:18 -0600

    Created a list and added to the list, added random

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -1,6 +1,17 @@
-using system;
-
-public class prompt 
+public class Prompt
 {
+    List<string> _prompt = new List<string>();
+    public string RandomPrompt()
+    {
+        _prompt.Add("Who was the most interesting person I interacted with today?");
+        _prompt.Add("What was the best part of my day?");
+        _prompt.Add("How did I see the hand of the Lord in my life today?");
+        _prompt.Add("What was the strongest emotion I felt today?");
+        _prompt.Add("If I had one thing I could do over today, what would it be?");
 
+         Random randomGenerator = new Random();
+         int index = randomGenerator.Next(_prompt.Count);
+
+         return _prompt[index];
+    }
 }
