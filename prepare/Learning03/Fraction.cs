f37b53df7122ff5f2912cb44b843bc5f5d649196
Peacho <emahilmo@>
2023-09-28 21:18:17 -0600

    Added constructor and added top and bottom

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -3,4 +3,16 @@ using System;
 public class Fraction{
     private int _top;
     private int _bottom;
+    public Fraction() {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber) {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom) {
+        _top = top;
+        _bottom = bottom;
+    }
 }
