633eb5c7058b4512f3e7d58f37567a9fc08277b6
Peacho <emahilmo@>
2023-10-03 17:42:34 -0600

    Got rid of _reference=reference and added setref

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -8,7 +8,7 @@ public class Reference {
     private int _endVerse;
 
     public Reference(string reference) {
-        _reference = reference;
+
     }
     public Reference(string book, int chapter, int verse) {
         _book = book;
@@ -16,6 +16,12 @@ public class Reference {
         _startVerse = verse;
     }
     public Reference(string book, int chapter, int verse, int endVerse) {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+        _endVerse = endVerse;
+    }
+    public void SetReference(string book, int chapter, int verse, int endVerse) {
         _book = book;
         _chapter = chapter;
         _startVerse = verse;
