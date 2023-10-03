e030ecce5481fd607596f249717e09a49f6b6dd9
Peacho <emahilmo@>
2023-10-03 17:32:56 -0600

    Added ref book chapter start/end verse and called

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,5 +1,28 @@
 using System;
 
 public class Reference {
-    
+    private string _reference;
+    private string _book;
+    private int _chapter;
+    private int _startVerse;
+    private int _endVerse;
+
+    public Reference(string reference) {
+        _reference = reference;
+    }
+    public Reference(string book, int chapter, int verse) {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+    }
+    public Reference(string book, int chapter, int verse, int endVerse) {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+        _endVerse = endVerse;
+    }
+    public string GetReferenceString(string book, int chapter, int verse, int endVerse) {
+        string reference = $"{_book} {_chapter} {_startVerse} {_endVerse}";
+        return reference;
+    }
 }
