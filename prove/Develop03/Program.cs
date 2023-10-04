c91e50df993e0ca18dcee5ac569dd925954d9646
Peacho <emahilmo@>
2023-10-03 19:12:56 -0600

    Changed the numbers in proverbs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs 3:5-6");
+        Reference reference = new Reference("Proverbs", 3, 5-6);
         string scriptureText = "Trust in the Lord with all your hear and lean not on your own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Scripture scripture = new(reference, scriptureText);
         Random random = new Random();
