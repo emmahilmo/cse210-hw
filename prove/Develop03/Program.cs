2a99093c7589d17c2831b25f211a4b0cb33e9c12
Peacho <emahilmo@>
2023-10-03 19:03:05 -0600

    Added the displays and the press enter or quit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,5 +7,9 @@ class Program
         Reference reference = new Reference("Proverbs 3:5-6");
         string scriptureText = "Trust in the Lord with all your hear and lean not on your own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Scripture scripture = new(reference, scriptureText);
+
+        Console.WriteLine(reference.GetReference());
+        Console.WriteLine(scripture.Display());
+        Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
     }
 }
