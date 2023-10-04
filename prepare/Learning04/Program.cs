072b3994e6044041974680a4030a4a31bcc272b7
Peacho <emahilmo@>
2023-10-04 10:35:39 -0600

    Added each assignment and changed names

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,9 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("Emma Hilmo", "Fractions");
-        MathAssignment mathAssignment = new MathAssignment("Emma Hilmo", "Fractions", "7.3", "3-7");
-        Console.WriteLine(assignment.GetSummary());
-        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Assignment a1 = new Assignment("Emma ", "Math");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Francis", "Fractions", "7.3", "3-7");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Jorge", "History", "WW2");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
