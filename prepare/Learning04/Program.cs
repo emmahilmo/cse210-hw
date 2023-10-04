b108723f1856ef795b62720e5997f9c5d4daed55
Peacho <emahilmo@>
2023-10-04 10:26:31 -0600

    Got rid of section and problems here

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Assignment assignment = new Assignment("Emma Hilmo", "Fractions");
-        MathAssignment mathAssignment = new MathAssignment("Emma Hilmo", "Fractions", "Section 7.3", "Problems 3-7");
+        MathAssignment mathAssignment = new MathAssignment("Emma Hilmo", "Fractions", "7.3", "3-7");
         Console.WriteLine(assignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
     }
