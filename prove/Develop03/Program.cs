8ccb76156e752f6f26cc9d09370041538d2f762f
Peacho <emahilmo@>
2023-10-03 18:34:11 -0600

    Added the reference.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Reference reference = new Reference("Proverbs 3:5");
+        string scriptureText = "Trust in the Lord with all your hear and lean not on your own understanding.";
+        Scripture scripture = new(reference, scriptureText);
     }
 }
