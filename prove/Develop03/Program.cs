ace322774016a0e78b861fbbb1d6e3db583dcbbb
Peacho <emahilmo@>
2023-10-03 19:23:06 -0600

    Added userinput and if statement

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string userinput = Console.ReadLine();
+        Reference reference = new Reference("Proverbs", 3, 5 , 6);
         string scriptureText = "Trust in the Lord with all your hear and lean not on your own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Scripture scripture = new(reference, scriptureText);
         Random random = new Random();
@@ -21,6 +22,9 @@ class Program
             Console.WriteLine(reference.GetReference());
             Console.WriteLine(scripture.Display());
             Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
+        }
+        if (userinput == "quit") {
+            Console.Clear();
         }
     }
 }
