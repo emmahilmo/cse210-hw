effc8a8bb832b387611a4c6966b49e25643df8af
Peacho <emahilmo@>
2023-10-03 18:34:50 -0600

    added word list and added to the list

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,5 +1,22 @@
 using System;
 
 public class Scripture {
-    
+    Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text) {
+        _reference = reference;
+        List<string> words =new List<string>(text.Split(' '));
+        foreach (var word in words) {
+            
+            Word word1 = new (word);
+            _words.Add(word1);
+        }
+    }
+    public int GetWord() {
+        return _words.Count();
+    }
+    public void HideRandomWord(int numberHide) {
+        _words[numberHide].Hide();
+    }
 }
