210d33225ab67d24dd07aa0e87adb5c470efb81e
Peacho <emahilmo@>
2023-10-03 19:35:13 -0600

    Added if statment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string userinput = Console.ReadLine();
-        Reference reference = new Reference("Proverbs", 3, 5 , 6);
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
         string scriptureText = "Trust in the Lord with all your hear and lean not on your own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Scripture scripture = new(reference, scriptureText);
         Random random = new Random();
@@ -14,17 +14,17 @@ class Program
         Console.WriteLine(scripture.Display());
         Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
 
-        while (Console.ReadKey().Key == ConsoleKey.Enter) {
-            int number = random.Next(scripture.GetWord());
-            scripture.HideRandomWord(number);
-            Console.Clear();
+        if (userinput != "quit") {
+
+            while (Console.ReadKey().Key == ConsoleKey.Enter) {
+                int number = random.Next(scripture.GetWord());
+                scripture.HideRandomWord(number);
+                Console.Clear();
             
-            Console.WriteLine(reference.GetReference());
-            Console.WriteLine(scripture.Display());
-            Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
-        }
-        if (userinput == "quit") {
-            Console.Clear();
+                Console.WriteLine(reference.GetReference());
+                Console.WriteLine(scripture.Display());
+                Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
+            }
         }
     }
 }
