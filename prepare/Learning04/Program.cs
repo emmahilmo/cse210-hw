29bc8c6b84ada11efa4e93b1eb6df01ca9d7aa71
Peacho <emahilmo@>
2023-10-04 10:15:26 -0600

    Added assignment back, added mathassignment + get

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("Emma Hilmo","Math");
+        Assignment assignment = new Assignment("Emma Hilmo", "Fractions");
+        MathAssignment mathAssignment = new MathAssignment("Emma Hilmo", "Fractions", "Section 7.3", "Problems 3-7");
         Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
