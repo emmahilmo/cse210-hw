2c62b1af34ee11f3579bef30a8c3bc207e027dfc
Peacho <emahilmo@>
2023-10-03 19:11:29 -0600

    Added new random and while loop

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,9 +7,20 @@ class Program
         Reference reference = new Reference("Proverbs 3:5-6");
         string scriptureText = "Trust in the Lord with all your hear and lean not on your own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
         Scripture scripture = new(reference, scriptureText);
+        Random random = new Random();
 
         Console.WriteLine(reference.GetReference());
         Console.WriteLine(scripture.Display());
         Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
+
+        while (Console.ReadKey().Key == ConsoleKey.Enter) {
+            int number = random.Next(scripture.GetWord());
+            scripture.HideRandomWord(number);
+            Console.Clear();
+            
+            Console.WriteLine(reference.GetReference());
+            Console.WriteLine(scripture.Display());
+            Console.WriteLine("Press 'enter' to continue or type 'quit' to end:");
+        }
     }
 }
