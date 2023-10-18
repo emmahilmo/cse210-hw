47f186c9f6ec159a61382363388722ba0cfb1aa2
Peacho <emahilmo@>
2023-10-18 17:32:36 -0600

    Added prompts as a list

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -2,7 +2,8 @@ using System;
 public class ListingActivity : Activity {
     private int _count;
     private List<string> _prompts;
-    public ListingActivity(string name, string description, int duration, int count, string prompts) : base(name, description, duration) {
+    public ListingActivity(string name, string description, int duration, int count, List<string> prompts) : base(name, description, duration) {
         _count = count;
+        _prompts = prompts;
     }
 }
