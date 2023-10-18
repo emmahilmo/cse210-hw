04a2f8c4bb4c54a1a47a77bd634fb62e4519ef76
Peacho <emahilmo@>
2023-10-18 17:32:24 -0600

    Added list for both questions and prompts

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -2,7 +2,9 @@ using System;
 public class ReflectingActivity : Activity {
     private List<string> _questions;
     private List<string> _prompts;
-    public ReflectingActivity(string name, string description, int duration, string questions, string prompts) : base(name, description, duration){
+    public ReflectingActivity(string name, string description, int duration, List<string> questions, List<string> prompts) : base(name, description, duration){
+        _questions = questions;
+        _prompts = prompts;
     }
 
 }
