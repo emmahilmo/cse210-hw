2fc0787bb1b6b46574ca89491445f556a535a564
Peacho <emahilmo@>
2023-10-19 15:36:03 -0600

    Seperated the prompt list adding to different way

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,20 +1,31 @@
 using System;
 public class ListingActivity : Activity {
     private int _count;
-    private List<string> _prompts;
+    private List<string> _prompts = new List<string>();
     public ListingActivity(string name, string description) : base(name, description) {
     }
-    private string GetRandomPrompt() {
-        _prompts.Add("Who are people that you appreciate?");
-        _prompts.Add("What are personal strengths of yours?");
-        _prompts.Add("Who are people that you have helped this week?");
-        _prompts.Add("When have you felt the Holy Ghost this month?");
-        _prompts.Add("Who are some of your personal heroes?");
+    private void GetRandomPrompt() {
+        Random randomGenerator = new Random();
+        int randomPrompt = randomGenerator.Next(_prompts.Count);
+        string prompt = _prompts[randomPrompt];
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine($" --- {prompt} --- ");
+        Console.WriteLine($"You may begin in: ");
+        ShowCountDown(5);
 
-         Random randomGenerator = new Random();
-         int index = randomGenerator.Next(_prompts.Count);
+    }
+    public void SetPrompts() {
+        string prompt1 = "Who are people that you appreciate?";
+        string prompt2 = "What are personal strengths of yours?";
+        string prompt3 = "Who are people that you have helped this week?";
+        string prompt4 = "When have you felt the Holy Ghost this month?";
+        string prompt5 = "Who are some of your personal heroes?";
 
-         return _prompts[index];
+        _prompts.Add(prompt1);
+        _prompts.Add(prompt2);
+        _prompts.Add(prompt3);
+        _prompts.Add(prompt4);
+        _prompts.Add(prompt5);
     }
     private List<string> GetListFromUser() {
         List<string> userList = new List<string>();
@@ -34,10 +45,8 @@ public class ListingActivity : Activity {
         Console.Clear();
         Console.WriteLine($"Get Ready...");
         Pause();
-        Console.WriteLine("List as many responses you can to the following prompt:");
-        Console.WriteLine($" --- {GetRandomPrompt()} --- ");
-        Console.WriteLine($"You may begin in: ");
-        ShowCountDown(5);
+        SetPrompts();
+        GetRandomPrompt();
         _count = GetListFromUser().Count;
         Console.WriteLine($"You have listed {_count} items!");
         Pause();
