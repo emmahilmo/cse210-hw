8d6d1a7db822921571a8e5d4637d511358763c45
Peacho <emahilmo@>
2023-10-18 22:16:06 -0600

    Got rid of count and prompt

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -2,9 +2,7 @@ using System;
 public class ListingActivity : Activity {
     private int _count;
     private List<string> _prompts;
-    public ListingActivity(string name, string description, int duration, int count, List<string> prompts) : base(name, description, duration) {
-        _count = count;
-        _prompts = prompts;
+    public ListingActivity(string name, string description) : base(name, description) {
     }
     private string GetRandomPrompt() {
         _prompts.Add("Who are people that you appreciate?");
