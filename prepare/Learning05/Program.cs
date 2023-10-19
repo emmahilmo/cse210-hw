11bb8c9d7200a8de3dd2d095e0900a638569444d
Peacho <emahilmo@>
2023-10-19 17:07:41 -0600

    Added each of the outputs for each shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("yellow", 2);
+        
+        Console.WriteLine(s1.GetColor());
+        Console.WriteLine(s1.GetArea());
+
+        Rectangle s2 = new Rectangle("Purple", 3, 2);
+        Console.WriteLine(s2.GetColor());
+        Console.WriteLine(s2.GetArea());
+
+        Circle s3 = new Circle("Blue", 2);
+        Console.WriteLine(s3.GetColor());
+        Console.WriteLine(s3.GetArea());
+
+        // Console.WriteLine($"The {color} shape has the area of {area}");
     }
 }
