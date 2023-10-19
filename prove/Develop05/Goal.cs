42227bdffe0c7684b1f9179040095203230aafef
Peacho <emahilmo@>
2023-10-19 17:43:45 -0600

    Added all the attributes

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,31 @@
+using System;
+public abstract class Goal {
+    private string _name;
+    private string _description;
+    private int _points;
+    public Goal(string name, string description, int points) {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+    public string GetName() {
+        return _name;
+    }
+    public string GetDescription() {
+        return _description;
+    }
+    public int GetPoints() {
+        return _points;
+    }
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public string GetStringDetails() {
+        string status = "[ ]";
+
+        if(IsComplete() == true ) {
+            status = "[X]";
+        }
+        return $"{status} {GetName()} {GetDescription()}";
+    }
+    public abstract string GetStringRepresentation();
+}
