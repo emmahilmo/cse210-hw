8f5bb6cfb74224c0be213b9f9d435a02593c9014
Peacho <emahilmo@>
2023-10-19 15:48:58 -0600

    Put prompts and questions in own area

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -1,43 +1,59 @@
 using System;
 public class ReflectingActivity : Activity {
-    private List<string> _questions = new List<string>();
-    private List<string> _prompts = new List<string>();
+    private List<string> _questions;
+    private List<string> _prompts;
     public ReflectingActivity(string name, string description) : base(name, description) {
     }
     private string GetRandomPrompt() {
          Random randomGenerator = new Random();
-         int index = randomGenerator.Next(_prompts.Count);
-
-        _prompts.Add("Think of a time when you stood up for someone else.");
-        _prompts.Add("Think of a time when you did something really difficult.");
-        _prompts.Add("Think of a time when you helped someone in need.");
-        _prompts.Add("Think of a time when you did something truly selfless.");
-
-         return _prompts[index];
+         int randomPrompt = randomGenerator.Next(_prompts.Count);
+         return _prompts[randomPrompt];
     }
     private string GetRandomQuestions() {
          Random randomGenerator = new Random();
-         int index = randomGenerator.Next(_questions.Count);
+         int randomQuestions = randomGenerator.Next(_questions.Count);
+         return _questions[randomQuestions];
+    }
+    public void SetPrompts() {
+        List<string> prompts = new List<string>();
+        string prompt1 = "Think of a time when you stood up for someone else";
+        string prompt2 = "Think of a time when you did something really difficult";
+        string prompt3 = "Think of a time when you helped someone in need";
+        string prompt4 = "Think of a time when you did something truly selfless";
 
-        _questions.Add("> Why was this experience meaningful to you?");
-        _questions.Add("> Have you ever done anything like this before?");
-        _questions.Add("> How did you get started?");
-        _questions.Add("> How did you feel when it was complete?");
-        _questions.Add("> What made this time different than other times when you were not as successful?");
-        _questions.Add("> What is your favorite thing about this experience?");
-        _questions.Add("> What could you learn from this experience that applies to other situations?");
-        _questions.Add("> What did you learn about yourself through this experience?");
-        _questions.Add("> How can you keep this experience in mind in the future?");
+        _prompts.Add(prompt1);
+        _prompts.Add(prompt2);
+        _prompts.Add(prompt3);
+        _prompts.Add(prompt4);
+    }
+    public void SetQuestions() {
+        string q1 = "> Why was this experience meaningful to you?";
+        string q2 = "> Have you ever done anything like this before?";
+        string q3 = "> How did you get started?";
+        string q4 = "> How did you feel when it was complete?";
+        string q5 = "> What made this time different than other times when you were not as successful?";
+        string q6 = "> What is your favorite thing about this experience?";
+        string q7 = "> What could you learn from this experience that applies to other situations?";
+        string q8 = "> What did you learn about yourself through this experience?";
+        string q9 = "> How can you keep this experience in mind in the future?";
 
-         return _questions[index];
+        _questions.Add(q1);
+        _questions.Add(q2);
+        _questions.Add(q3);
+        _questions.Add(q4);
+        _questions.Add(q5);
+        _questions.Add(q6);
+        _questions.Add(q7);
+        _questions.Add(q8);
+        _questions.Add(q9);
     }
     public void DisplayPrompt() {
         Console.WriteLine("Consider the following prompt:");
         Console.WriteLine($" --- {GetRandomPrompt()} --- ");
+        Console.WriteLine("When you have something in mind, press enter to continue.");
     }
     public void DisplayQuestion() {
         Console.Clear();
-
         int seconds = GetDuration();
         DateTime startTime = DateTime.Now;
         DateTime futureTime = startTime.AddSeconds(seconds);
@@ -52,8 +68,9 @@ public class ReflectingActivity : Activity {
         Console.Clear();
         Console.WriteLine($"Get Ready...");
         Pause();
+        SetPrompts();
+        SetQuestions();
         DisplayPrompt();
-        Console.WriteLine("When you have something in mind, press enter to continue.");
         if (Console.ReadKey().Key == ConsoleKey.Enter) {
             Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
             Console.WriteLine($"You may begin in: ");
