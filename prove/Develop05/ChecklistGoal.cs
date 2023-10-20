6baf8c9bf6de618bc63d58e89394dc45553e9f15
Peacho <emahilmo@>
2023-10-19 20:14:30 -0600

    put that first, added record event and fixed error

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,8 +1,8 @@
 using System;
 public class ChecklistGoal : Goal {
-    private int _amountCompleted = 0;
     private int _target;
     private int _bonus;
+    private int _amountCompleted = 0;
     public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points) {
         _target = target;
         _bonus = bonus;
@@ -26,18 +26,34 @@ public class ChecklistGoal : Goal {
         _bonus = bonus;
     }
     public override void RecordEvent() {
-        
+        _amountCompleted += 1;
+
+        if (IsComplete() == true) {
+            
+            int bonus = GetPoints() + _bonus;       
+        }
     }
      public override bool IsComplete() {
-        return false;
+        
+        if (_amountCompleted == _target) {
+            return true;
+        }
+        else {return false;}
     }
     public override void SetComplete(bool value) {
-        
+        _amountCompleted = 0;
     }
-    // public override string GetDetailsString() {
+    public override string GetStringDetails() {
+        string status = "[ ]";
+
+        if (IsComplete() == true)
+        {
+            status = "[X]";
+        }
         
-    // }
+        return $"{status} {GetName()} ({GetDescription()} -- Currently completed: {GetAmountCompleted()}/{GetTarget()})";
+    }
     public override string GetStringRepresentation() {
-        return $"ChecklistGoal: {GetName()}, {GetDescription()}, {GetPoints()}, {GetAmountCompleted()}, {GetTarget()}, {GetBonus()}";
+        return $"ChecklistGoal, {GetName()}, {GetDescription()}, {GetPoints()}, {GetAmountCompleted()}, {GetTarget()}, {GetBonus()}";
     }
 }
