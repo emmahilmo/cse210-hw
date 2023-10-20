e3e4949a46747896c29b6efaa0952f4fd44920b0
Peacho <emahilmo@>
2023-10-19 18:07:13 -0600

    Set up base for the program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int selection = -1;
+        while(selection != 6) {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+            Console.WriteLine("Select a choice from the menu: ");
+            selection = int.Parse(Console.ReadLine());
+
+            switch(selection) {
+                case 1: {
+                    // create new goal
+
+                    break;
+                }
+                case 2: {
+                    // list goals
+
+                    break;
+                }
+                case 3: {
+                    // save goals
+
+                    break;
+                }
+                case 4: {
+                    // load goals
+
+                    break;
+                }
+                case 5: {
+                    // record event
+                    
+                    break;
+                }
+            }
+        }
     }
 }
