fb89d2e725ac681198360424b904c105e008e42d
Peacho <emahilmo@>
2023-10-19 18:08:46 -0600

    Add class record iscomplete setcomplete getstring

---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,20 @@
+using System;
+public class SimpleGoal : Goal {
+    private bool _isComplete = false;
+    public SimpleGoal(string name, string description, int points, bool complete) : base(name, description, points) {
+    }
+    public override void RecordEvent()
+    {
+        _isComplete = true;
+    }
+    public override bool IsComplete()
+    {
+        return IsComplete();
+    }
+    public override void SetComplete(bool value) {
+        _isComplete = value;
+    }
+    public override string GetStringRepresentation() {
+        return $"Simple Goal: {GetName()}, {GetDescription()}, {GetPoints()}, {IsComplete()}";
+    }
+}
