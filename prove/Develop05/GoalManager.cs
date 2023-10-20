64452c2a25e25c741df6123cc6deec8cefe934ed
Peacho <emahilmo@>
2023-10-19 18:44:34 -0600

    added list, start, display and create

---
+++ prove/Develop05/GoalManager.cs
@@ -0,0 +1,17 @@
+using System;
+public class GoalManager {
+    private List<Goal> _goals = new List<Goal>();
+    private int _score;
+    public GoalManager(int score) {
+        _score = score;
+    }
+    public void start() {
+
+    }
+    public void DisplayPlayerInfo() {
+
+    }
+    public void CreateGoal() {
+
+    }
+}   
