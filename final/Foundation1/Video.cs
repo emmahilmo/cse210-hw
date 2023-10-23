df5292b199a72a63c81cb0b3be6b55f9995ba619
Peacho <emahilmo@>
2023-10-23 15:12:44 -0600

    Added attributes and display

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,16 @@
+using System;
+public class Video {
+    public string _title;
+    public string _author;
+    public int _length;
+    public List<Comments>_comments = new List<Comments>();
+    
+    public void Display() {
+        Console.WriteLine($"{_title} - {_author} - {_length}(in seconds)");
+        Console.WriteLine($"Comment: {_comments}");
+
+        foreach(Comments comments in _comments) {
+            comments.Display();
+        }
+    }
+}
