ee947382b94f36e3d0665aed8dfc7c6cabb55acf
Peacho <emahilmo@>
2023-10-23 15:12:29 -0600

    Added attributes and display

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,10 @@
+using System;
+public class Comments {
+    public string _name;
+    public string _text;
+
+    public void Display() {
+        Console.WriteLine($"{_name}");
+        Console.WriteLine($"{_text}");
+    }
+}
