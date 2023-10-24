91f9b14235b6b688bb516dcdf8b1490915b62793
Peacho <emahilmo@>
2023-10-24 13:26:55 -0600

    Added reception class

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -1,4 +1,10 @@
 using System;
 public class Reception : Event {
-    
+    public string _RSVP;
+    public Reception(string title, string description, string date, string time, Address address, string RSVP) : base(title, description, date, time, address) {
+        _RSVP = RSVP;
+    }
+    public string GetRSVP() {
+        return _RSVP;
+    }
 }
