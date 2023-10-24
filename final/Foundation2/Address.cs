c0a1581df140e2e712785cfc2264f493113eccf1
Peacho <emahilmo@>
2023-10-23 22:31:22 -0600

    Added necessary stuff

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,26 @@
+using System;
+public class Address {
+    private string _street;
+    private string _city;
+    private string _stateProvence;
+    private string _country;
+    private bool _USA = false;
+    public Address(string street, string city, string stateProvence, string country)  {
+        _street = street;
+        _city = city;
+        _stateProvence = stateProvence;
+        _country = country;
+    }
+    public void inUSA() {
+        _USA = true;
+    }
+    public void isntUSA() {
+        _USA = false;
+    }
+    public bool isInUSA() {
+        return _USA;
+    }
+    public string GetAddress() {
+        return $"{_street} {_city}, {_stateProvence} {_country}";
+    }
+}
