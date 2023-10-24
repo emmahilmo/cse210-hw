e3c008d0f47ca9d824139750cdde4605a83e002b
Peacho <emahilmo@>
2023-10-23 22:41:45 -0600

    Added strings for event to address

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,9 @@
+using System;
+public class Event {
+    private string _eventTitle;
+    private string _description;
+    private string _date;
+    private string _time;
+    private string _address;
+    
+}
