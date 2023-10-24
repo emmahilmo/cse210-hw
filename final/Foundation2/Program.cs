110bd7282a49160838bc474191ab2b7d51e48b04
Peacho <emahilmo@>
2023-10-24 09:29:28 -0600

    Added base product

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Product product1 = new Product("Chocklate Milk", 1300, 3, 2);
+        Product product2 = new Product("Yogurt", 192, 2, 1);
+        // Customer customer1 = new Customer("Jamie" );
+        Console.WriteLine();
     }
 }
