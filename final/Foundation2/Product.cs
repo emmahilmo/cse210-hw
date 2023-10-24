4644ff2055425b4e9b35729a5965c0980efd0bf7
Peacho <emahilmo@>
2023-10-24 09:29:14 -0600

    Changed price to float and added getproduct

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -2,15 +2,18 @@ using System;
 public class Product {
     private string _name;
     private int _id;
-    private int _price;
+    private float _price;
     private int _quantity;
-    public Product(string name, int id, int price, int quantity) {
+    public Product(string name, int id, float price, int quantity) {
         _name = name;
         _id = id;
         _price = price;
         _quantity = quantity;
     }
-    public string ComputePrice() { 
+    public string computePrice() { 
         return $"{_price}*{_quantity}";
+    }
+    public string GetProduct() {
+        return $"{_name} {_id}";
     }
 }
