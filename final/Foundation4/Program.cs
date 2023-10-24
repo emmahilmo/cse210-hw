d39d93142638e0a4ed5aebedd17fa3dc79031d7f
Peacho <emahilmo@>
2023-10-24 15:47:47 -0600

    Finished Class

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+        Running running = new Running("Nov 03 2023", 30, 3);
+        activities.Add(running);
+
+        Cycling cycling = new Cycling("Nov 05 2023", 30, 6);
+        activities.Add(cycling);
+
+        Swimming swimming = new Swimming("Nov 07 2023", 30, 10);
+        activities.Add(swimming);
+        
+
+        foreach(Activity activity in activities) {
+            Console.WriteLine(activity.GetSummary());
+        
+        }
     }
 }
