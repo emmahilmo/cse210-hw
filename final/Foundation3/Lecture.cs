e71ad1e9abcff6f82ab698f5db7c560eedcb1416
Peacho <emahilmo@>
2023-10-24 13:26:17 -0600

    Added lecture class

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,4 +1,15 @@
 using System;
 public class Lecture : Event {
-    
+    private string _speaker;
+    private int _capacity;
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity) : base(title, description, date, time, address) {
+        _capacity = capacity;
+        _speaker = speaker;
+    }
+    public int GetCapacity() {
+        return _capacity;
+    }
+    public string GetSpeaker() {
+        return _speaker;
+    }
 }
