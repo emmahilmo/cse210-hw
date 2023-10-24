858448fd3e3b651a25c8e9d12f5d3d92252543a0
Peacho <emahilmo@>
2023-10-24 13:26:31 -0600

    Added outdoorgathering class

--- final/Foundation3/OutdoorGathering.cs
+++ final/Foundation3/OutdoorGathering.cs
@@ -1,4 +1,10 @@
 using System;
 public class OutdoorGathering : Event {
-    
+    private string _weather;
+    public OutdoorGathering(string title, string description, string date, string time, Address address, string weather) : base(title, description, date, time, address) {
+        _weather = weather;
+    }
+    public string GetWeather() {
+        return _weather;
+    }
 }
