0d4e40d2598febda0579bbfae0fa60353ae0568b
Peacho <emahilmo@>
2023-10-24 13:26:06 -0600

    Added getters for each

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -4,6 +4,28 @@ public class Event {
     private string _description;
     private string _date;
     private string _time;
-    private string _address;
-    
+    Address _address;
+    public Event(string title, string description, string date, string time, Address address) {
+        _eventTitle = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+    public string GetTitle() {
+        return _eventTitle;
+    }
+    public string GetDescription() {
+        return _description;
+    }
+    public string GetDate() {
+        return _date;
+    }
+    public string GetTime() {
+        return _time;
+    }
+    public string GetAddress() {
+        return _address.GetAddress();
+    }
+
 }
