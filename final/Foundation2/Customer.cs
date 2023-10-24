ab2fbb5a3a6339e946262098b4b1fb3bd6922573
Peacho <emahilmo@>
2023-10-24 09:27:58 -0600

    added shipping, and getcustomer

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,17 +1,20 @@
 using System;
 public class Customer {
-    private string _customerName;
-    Address _address;
-    public Customer(string name, Address address) {
-        _customerName = name;
-        _address = address;
+    private string _name;
+    private int _shipping;
+    Address GetAddress;
+    public Customer(string name) {
+        _name = name;
     }
     public void isUSACustomer() {
-        if(_address.isInUSA() == true) {
-            
+        if(GetAddress.isInUSA() == true) {
+            _shipping = 5;
         }
         else {
-
+            _shipping = 35;
         }
+    }
+    public string GetCustomer() {
+        return $"{_name} {GetAddress}";
     }
 }
